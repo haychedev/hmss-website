a40cb063cb045c0debb295a2716b488e53a539da
w3wiz-dev <w3wiz1@gmail.com>
2023-11-09 17:39:36 +0100

    fixed

--- src/pages/who we are/mainSection.vue
+++ src/pages/who we are/mainSection.vue
@@ -77,11 +77,11 @@
                     <!-- <a href="/defense-pdf">
                         Download TTSS program info
                     </a> -->
-                    <!-- <a href="https://www.docdroid.net/file/download/suBR05D/womens-self-defense-1-pdf.pdf" download="women.pdf">Download TTSS program info</a> -->
+                    <a href="https://www.docdroid.net/file/download/suBR05D/womens-self-defense-1-pdf.pdf" download="women.pdf">Download TTSS program info</a>
 
-                    <a href="../../assets/WOMENS-SELF-DEFENSE.pdf" download>
+                    <!-- <a href="../../assets/WOMENS-SELF-DEFENSE.pdf" download>
                         <button class="btn btn-primary">Download TTSS program info</button>
-                    </a>
+                    </a> -->
 
 
 
